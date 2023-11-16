eedf5a0fe3f42272310f1dcd22372e60cd34f7f8
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-15 21:31:28 -0300

    test: ensure RemoteAuthentication throws UnexpectedError if HttpPostClient returns 404

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -52,4 +52,13 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('Should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
