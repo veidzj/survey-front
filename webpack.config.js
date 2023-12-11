728030cd78680338cacc40b5ed0b4cc5e9429a87
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-10 23:42:54 -0300

    fix: webpack filename

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
-    fileName: 'bundle.js'
+    filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'scss'],
