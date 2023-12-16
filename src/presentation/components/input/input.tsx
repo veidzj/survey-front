4ccaf95ec467545ccc23e46909c290c8db6e618d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-16 12:25:39 -0300

    feat: ensure Login shows valid password state if Validation succeeds

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -20,11 +20,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo certo!'
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   return (
